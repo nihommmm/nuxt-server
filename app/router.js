6f52ca898aa2fba01aa9671412ff15b4784706ad
nihommmm <litong.yan@happy-seed.com>
2022-02-08 17:29:19 +0800

    新增egg几个辅助插件以及user接口分组和验证码初步实现

--- app/router.js
+++ app/router.js
@@ -6,4 +6,14 @@
 module.exports = app => {
   const { router, controller } = app
   router.get('/', controller.home.index)
+  // 验证码
+  router.get('/captcha', controller.util.captcha)
+
+  router.group({ name: 'user', prefix: '/user' }, router => {
+    const { info, register, login, verify } = controller.user
+    router.get('/info', info)
+    router.post('/register', register)
+    router.post('/login', login)
+    router.get('/verify', verify)
+  })
 }
