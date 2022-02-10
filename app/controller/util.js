b25d7c42ca68078a542a5cfabf4615032f727883
nihommmm <litong.yan@happy-seed.com>
2022-02-10 17:44:23 +0800

    【新增】切片上传功能以及文件合并接口

--- app/controller/util.js
+++ app/controller/util.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const svgCaptcha = require('svg-captcha')
+const path = require('path')
 const BaseController = require('./base')
 const fse = require('fs-extra')
 
@@ -43,10 +44,31 @@ class UtilController extends BaseController {
     const { ctx } = this
     console.log(ctx.request, 'aaaa==>aaaa')
     const file = ctx.request.files[0]
-    const { name } = ctx.request.body
-    console.log(file, name)
-    await fse.move(file.filepath, this.config.UPLOAD_DIR + '/' + file.filename)
-    this.success({ url: `/public/${file.filename}` })
+    const { hash, name } = ctx.request.body
+    const chunkPath = path.resolve(this.config.UPLOAD_DIR, hash)
+
+    // const filePath = path.resolve() // 文件最终存储的位置。合并之后
+
+    // console.log(name,file)
+
+    // console.log(file.filepath)
+    // console.log(this.config.UPLOAD_DIR)
+    if (!fse.existsSync(chunkPath)) {
+      await fse.mkdir(chunkPath)
+    }
+
+    await fse.move(file.filepath, `${chunkPath}/${name}`)
+
+    this.message('切片上传成功')
+  }
+
+  async mergefile() {
+    const { ext, size, hash } = this.ctx.request.body
+    const filePath = path.resolve(this.config.UPLOAD_DIR, `${hash}.${ext}`)
+    await this.ctx.service.tools.mergeFile(filePath, hash, size)
+    this.success({
+      url: `/public/${hash}.${ext}`,
+    })
   }
 }
 
--- app/service/tools.js
+++ app/service/tools.js
@@ -1,8 +1,8 @@
 'use strict'
 
 const { Service } = require('egg')
-// const path = require('path')
-// const fse = require('fs-extra')
+const path = require('path')
+const fse = require('fs-extra')
 const nodemailer = require('nodemailer')
 
 
@@ -18,33 +18,33 @@ const transporter = nodemailer.createTransport({
 
 
 class ToolService extends Service {
-//   async mergeFile(filepPath, filehash, size) {
-//     const chunkdDir = path.resolve(this.config.UPLOAD_DIR, filehash) // 切片的文件夹
-//     let chunks = await fse.readdir(chunkdDir)
-//     chunks.sort((a, b) => a.split('-')[1] - b.split('-')[1])
-//     chunks = chunks.map(cp => path.resolve(chunkdDir, cp))
-//     await this.mergeChunks(chunks, filepPath, size)
+  async mergeFile(filepPath, filehash, size) {
+    const chunkdDir = path.resolve(this.config.UPLOAD_DIR, filehash) // 切片的文件夹
+    let chunks = await fse.readdir(chunkdDir)
+    chunks.sort((a, b) => a.split('-')[1] - b.split('-')[1]) // 排序 需要严格按照数字排序
+    chunks = chunks.map(cp => path.resolve(chunkdDir, cp))
+    await this.mergeChunks(chunks, filepPath, size)
 
-  //   }
-  //   async mergeChunks(files, dest, size) {
-  //     const pipStream = (filePath, writeStream) => new Promise(resolve => {
-  //       const readStream = fse.createReadStream(filePath)
-  //       readStream.on('end', () => {
-  //         fse.unlinkSync(filePath)
-  //         resolve()
-  //       })
-  //       readStream.pipe(writeStream)
-  //     })
+  }
+  async mergeChunks(files, dest, size) {
+    const pipStream = (filePath, writeStream) => new Promise(resolve => {
+      const readStream = fse.createReadStream(filePath)
+      readStream.on('end', () => {
+        fse.unlinkSync(filePath)
+        resolve()
+      })
+      readStream.pipe(writeStream)
+    })
 
-  //     await Promise.all(
-  //       files.forEach((file, index) => {
-  //         pipStream(file, fse.createWriteStream(dest, {
-  //           start: index * size,
-  //           end: (index + 1) * size,
-  //         }))
-  //       })
-  //     )
-  //   }
+    await Promise.all(
+      files.map((file, index) =>
+        pipStream(file, fse.createWriteStream(dest, {
+          start: parseInt(index * size),
+          end: parseInt((index + 1) * size),
+        }))
+      )
+    )
+  }
   async sendMail(email, subject, text, html) {
     console.log(email, subject, html)
     const mailOptions = {
